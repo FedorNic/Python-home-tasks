4047a79bae289ff20d6939c89028f46fdebf55eb
FedorNic <dmitrievfedor@yandex.ru>
2022-04-17 21:29:44 +0300

    14

--- 7.py
+++ 7.py
@@ -1,18 +1,22 @@
 # 7. Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
-def logic(x, y, z, f):
-    if (not (x or y or z) == (not x and not y and not z)):
-        f = True
-    print(f)
+from unittest import result
 
-f1, f2, f3, f4, f5, f6, f7, f8 = False, False, False, False, False, False, False, False
-logic(True, True, True, f1)
-logic(True, True, False, f2)
-logic(True, False, True, f3)
-logic(False, True, True, f4)
-logic(True, False, False, f5)
-logic(False, False, True, f6)
-logic(False, True, True, f7)
-logic(False, False, False, f8)
+
+def logic(x, y, z):
+    if (not (x or y or z) == (not x and not y and not z)):
+        result = True
+    else: result = False
+    print(result)
+    return result
+
+f1= logic(True, True, True)
+f2= logic(True, True, False)
+f3 =logic(True, False, True)
+f4 = logic(False, True, True)
+f5 = logic(True, False, False)
+f6 = logic(False, False, True)
+f7 = logic(False, True, True)
+f8 = logic(False, False, False)
 
 if (all([f1, f2, f3, f4, f5, f6, f7, f8])) == True:
     print('Выражение тождественно')
