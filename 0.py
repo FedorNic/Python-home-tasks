c72a8bad9b658ca4127c3b4efe792f7dbf1dceb3
FedorNic <dmitrievfedor@yandex.ru>
2022-06-07 20:04:49 +0300

    07.06.2022

--- 0.py
+++ 0.py
@@ -1,5 +1,17 @@
 # 0. Вывести квадрат числа
-a = int(input('Введите число \n'))
-print(f'Квадрат числа {a} равен {a**2}')
+# a = int(input('Введите число \n'))
+# print(f'Квадрат числа {a} равен {a**2}')
 
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import interpolate
+q=63000
+x = [31, 42, 51, 61, 76]
+y = [19240, 25000, 30500, 55555, 65000]
+f = interpolate.interp1d(x, y, fill_value="extrapolate")
+# x1 = np.arange(6, 12)
+# y1 = f(x1)   # использовать функцию интерполяции, возвращаемую `interp1d`
+
+plt.plot(x, y, 'o', x, y, '--')
+plt.show()
 
